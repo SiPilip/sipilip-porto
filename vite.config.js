7d6152c40a382272a259045b6d2738f9ec08b31d
SiPilip <philifs.74hutar@gmail.com>
2026-01-29 02:34:47 +0700

    feat: Enable strict port enforcement for Vite dev server.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 6000,
-    host: "0.0.0.0", // Sangat penting agar bisa diakses via IP VPS
+    host: "0.0.0.0", // Ini krusial agar bisa diakses secara network
+    strictPort: true,
   },
-  plugins: [react()],
 });
