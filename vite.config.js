363c251b23bdb7955b6e2bb76af44b7382bc41f9
SiPilip <philifs.74hutar@gmail.com>
2026-01-29 02:31:13 +0700

    config: Configure Vite dev server to listen on port 6000 and host 0.0.0.0 for external access.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 6000,
+    host: "0.0.0.0", // Sangat penting agar bisa diakses via IP VPS
+  },
   plugins: [react()],
 });
